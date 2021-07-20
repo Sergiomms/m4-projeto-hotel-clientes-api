e76f1de489783b45c346aaf2da3ef15bd80fa1fe
Sergiomms <mariano.srgio@gmail.com>
2021-07-20 16:02:44 -0300

    model de cliente criado

---
+++ src/model/ClienteModel.js
@@ -0,0 +1,12 @@
+class Cliente{
+    constructor(nome, email, cpf, quarto, checkIn, checkOut){
+        this.nome = nome,
+        this.email = email,
+        this.cpf = cpf,
+        this.quarto = quarto,
+        this.checkIn = checkIn,
+        this.checkOut = checkOut
+    }
+}
+
+module.exports = Cliente
