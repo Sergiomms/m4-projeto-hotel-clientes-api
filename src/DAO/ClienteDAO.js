e509e50208bf5dd0ecb639215f1aaf45838660a7
Sergiomms <mariano.srgio@gmail.com>
2021-07-22 15:10:31 -0300

    Rota PUT criada e rota DELETE criada

--- src/DAO/ClienteDAO.js
+++ src/DAO/ClienteDAO.js
@@ -14,6 +14,17 @@ class ClienteDAO{
             })
         })
     }
+    getClientFromEmail(email){
+        return new Promise((resolve, reject) =>{
+            this.db.all("SELECT * FROM CLIENTES WHERE EMAIL = (?)", email, (err, rows) =>{
+                if(err){
+                    reject(err)
+                }else{
+                    resolve(rows)
+                }
+            })
+        })
+    }
     insertClients(cliente){
         return new Promise((resolve, reject) =>{
             this.db.run("INSERT INTO CLIENTES(NOME, EMAIL, CPF, QUARTO) VALUES (?,?,?,?)", Object.values(cliente), (err) =>{
@@ -21,6 +32,58 @@ class ClienteDAO{
                     reject(err)
                 }else{
                     resolve()
+                }
+            })
+        })
+    }
+    deleteClientFromEmail(email){
+        return new Promise((resolve, reject) =>{
+            this.db.all("DELETE FROM CLIENTES WHERE EMAIL = (?)", email, (err) =>{
+                if(err){
+                    reject(err)
+                }else{
+                    resolve()
+                }
+            })
+        })
+    }
+    updateClient(nome, email, cpf, quarto, id){
+       
+        return new Promise((resolve, reject) =>{
+
+            let params = [id]
+            let query = "UPDATE CLIENTES SET"
+            let dados = []
+            let changes = 0;
+
+            if(nome != null){
+                params.unshift(nome)
+                dados.unshift(" NOME =?")
+                ++changes
+            }
+            if(email != null){
+                params.unshift(email)
+                dados.unshift(" EMAIL =?")
+                ++changes
+            }
+            if(cpf != null){
+                params.unshift(cpf)
+                dados.unshift(" CPF =?")
+                ++changes
+            }
+            if(quarto != null){
+                params.unshift(quarto)
+                dados.unshift(" QUARTO =?")
+                ++changes
+            }
+            
+            query += dados.join(',') + " WHERE ID = ?"
+
+            this.db.run(query, params, (err) =>{
+                if(err){
+                    reject(err)
+                }else{
+                    resolve({Changes:changes})
                 }
             })
         })
--- src/controller/cliente_controller.js
+++ src/controller/cliente_controller.js
@@ -18,8 +18,22 @@ module.exports = (app, db) => {
             response.json({err})
         })
     })
+    app.get('/clientes/:email', (request, response) =>{
+        const email = request.params.email
+        clienteDb.getClientFromEmail(email)
+        .then((rows) => {
+            response.json({
+                result:rows,
+                count:rows.length
+            })
+        })
+        .catch((err) => {
+            response.json({err})
+        })
+    })
     app.post('/clientes', (request, response)=>{
         const {nome, email, cpf, quarto} = request.body
+        
         let newClient = new Cliente(nome, email, cpf, quarto)
         clienteDb.insertClients(newClient)
         .then(()=>{
@@ -35,8 +49,37 @@ module.exports = (app, db) => {
             })
         })
     })
-
-
+    app.delete('/clientes/:email', (request, response) =>{
+        const email = request.params.email
+        clienteDb.deleteClientFromEmail(email)
+        .then(() =>{
+            response.json({
+                message:"Cliente deletado com sucesso"
+            })
+        })
+        .catch((err) =>{
+            response.json({
+                message:"Erro ao deletar cliente",
+                Error:err
+            })
+        })
+    })
+    app.put('/clientes/:id', (request, response) =>{
+        const id = request.params.id
+        const {nome, email, cpf, quarto} = request.body
+        clienteDb.updateClient(nome, email, cpf, quarto, id)
+        .then(() =>{
+            response.json({
+                Message:"Alteração realizada com sucesso"
+            })
+        })
+        .catch((err) =>{
+            response.json({
+                message:"Erro ao alterar cliente",
+                Error:err
+            })
+        })    
+    })
 
 }
 
