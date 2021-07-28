7cbe176efc576e87eb0ac21e941c438cc32b5f64
Sergiomms <mariano.srgio@gmail.com>
2021-07-28 15:23:59 -0300

    cors add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const porta = process.env.PORT || 3089
+const cors = require('cors')
 
 const mostraCliente = require("./controller/cliente_controller")
 
 const db = require('./infra/sqlite-db')
 
 app.use(express.json())
+app.use(cors())
 
 mostraCliente(app, db)
 
